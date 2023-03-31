000c52be29e37c9382f3cbc973a1e4e1334634c3
naingolan <kelvinsdechaw@gmail.com>
2023-03-31 04:34:07 +0300

    New project

---
+++ BookListDRF/serializers.py
@@ -0,0 +1,8 @@
+from .models import *
+from rest_framework import serializers
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'author', 'price']
+    
